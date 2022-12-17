2040d68be9b45d6588f141a86f7b15bf22a66eb5
YingWgithub <yingwang6333@gmail.com>
2022-12-17 12:47:50 -0500

    121722

--- practice/code1.R
+++ practice/code1.R
@@ -1,1 +1,13 @@
 ##### 12-15-2022 code1 ####
+
+# install shiny 
+install.packages("shiny")
+library(shiny)
+
+# example 1
+ui <- fluidPage(
+  "Hello, world!"
+)
+server <- function(input, output, session) {
+}
+shinyApp(ui, server)
