28c2cccced327516767388a4ba2bf53a739be1ca
YingWgithub <yingwang6333@gmail.com>
2022-12-19 18:29:01 -0500

    1219

--- practice/code1.R
+++ practice/code1.R
@@ -11,3 +11,51 @@ ui <- fluidPage(
 server <- function(input, output, session) {
 }
 shinyApp(ui, server)
+
+# example 2
+ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table"),
+  verbatimTextOutput("structure") # adding the information for "str", same format as "summary"
+)
+
+server <- function(input, output, session) {
+  output$summary <- renderPrint({
+    dataset <- get(input$dataset, "package:datasets")
+    summary(dataset)
+  })
+  
+  output$table <- renderTable({
+    dataset <- get(input$dataset, "package:datasets")
+    head(dataset) 
+  })
+  
+  output$structure <- renderPrint({
+    dataset <- get(input$dataset, "package:datasets")
+    str(dataset)
+  })
+}
+shinyApp(ui, server)
+
+# to improve the efficiency and reduce unwanted updates
+server2 <- function(input, output, session) {
+  # Create a reactive expression
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
+  output$summary <- renderPrint({
+    # Use a reactive expression by calling it like a function
+    summary(dataset())
+  })
+  
+  output$table <- renderTable({
+    head(dataset())
+  })
+  
+  output$structure <- renderPrint({
+    str(dataset())
+  })
+}
+shinyApp(ui, server2)
