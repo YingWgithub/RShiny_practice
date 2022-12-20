2fd2dc871e388686ccb02b25d08b80cd572f541c
YingWgithub <yingwang6333@gmail.com>
2022-12-19 22:23:01 -0500

    1219

--- practice/code1.R
+++ practice/code1.R
@@ -3,7 +3,6 @@
 # install shiny 
 install.packages("shiny")
 library(shiny)
-
 # example 1
 ui <- fluidPage(
   "Hello, world!"
@@ -19,18 +18,15 @@ ui <- fluidPage(
   tableOutput("table"),
   verbatimTextOutput("structure") # adding the information for "str", same format as "summary"
 )
-
 server <- function(input, output, session) {
   output$summary <- renderPrint({
     dataset <- get(input$dataset, "package:datasets")
     summary(dataset)
   })
-  
   output$table <- renderTable({
     dataset <- get(input$dataset, "package:datasets")
     head(dataset) 
   })
-  
   output$structure <- renderPrint({
     dataset <- get(input$dataset, "package:datasets")
     str(dataset)
@@ -44,18 +40,56 @@ server2 <- function(input, output, session) {
   dataset <- reactive({
     get(input$dataset, "package:datasets")
   })
-  
   output$summary <- renderPrint({
     # Use a reactive expression by calling it like a function
     summary(dataset())
   })
-  
   output$table <- renderTable({
     head(dataset())
   })
-  
   output$structure <- renderPrint({
     str(dataset())
   })
 }
 shinyApp(ui, server2)
+
+# example3
+ui <- fluidPage(
+  sliderInput("x", "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", "and y is", min = 1, max = 50, value = 5),
+  "then, (x * y) is", textOutput("product"),
+  "and, (x * y) + 5 is", textOutput("product_plus5"),
+  "and, (x * y) + 10 is", textOutput("product_plus10") 
+)
+server <- function(input, output, session) {
+  output$product <- renderText({
+    product <- input$x * input$y
+    product
+  })
+  output$product_plus5 <- renderText({
+    product <- input$x * input$y
+    product + 5
+  })
+  output$product_plus10 <- renderText({
+    priduct <- input$x * input$y
+    product + 10
+  })
+} # Error: object 'product' not found
+
+server <- function(input, output, session) {
+  product <-reactiveVal(NULL)
+  output$product <- renderText({
+    product <- input$x * input$y
+    product
+  })
+  output$product_plus5 <- renderText({
+    product <- input$x * input$y
+    product + 5
+  })
+  output$product_plus10 <- renderText({
+    priduct <- input$x * input$y
+    product + 10
+  })
+} # Error: non-numeric argument to binary operator
+shinyApp(ui,server)
+
